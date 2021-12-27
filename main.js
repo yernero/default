07781a610203a9a48930887a94f4e9d69ec56356
Yernero <jacobmajor@aol.com>
2021-12-26 19:03:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -69,17 +69,19 @@ module.exports.loop = function () {
 
     }
 
-    //keep track of creeps of each type
+    //Roles
     var fillers = _.filter(Game.creeps, (creep) => creep.memory.role == "filler");
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    var Hteam1 = _.filter(Game.creeps, (creep) => creep.memory.team == 1 && creep.memory.role == "harvester");
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
     var settlers = _.filter(Game.creeps, (creep) => creep.memory.role == "settler");
-    var Bteam1 = _.filter(Game.creeps, (creep) => creep.memory.team == 1 && creep.memory.role == "builder");
     var importers = _.filter(Game.creeps, (creep) => creep.memory.role == "importer");
     var towerGuards = _.filter(Game.creeps, (creep) => creep.memory.role == "towerGuard");
+
+    //Teams
+    var Hteam1 = _.filter(Game.creeps, (creep) => creep.memory.team == 1 && creep.memory.role == "harvester");
+    var Bteam1 = _.filter(Game.creeps, (creep) => creep.memory.team == 1 && creep.memory.role == "builder");
 
     //store pixels
     if (Game.time % 5 == 0) {
@@ -93,42 +95,49 @@ module.exports.loop = function () {
 
     //console.log(Game.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity}}));
 
-
+    //check ability to create new screep
     if (myroom.energyAvailable > 200) {
-        var towers = Game.spawns["HELL"].room.find(FIND_STRUCTURES, { filter: (structure) => structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity });
+        //see if room has towers
+        var towers = myroom.find(FIND_STRUCTURES, { filter: (structure) => structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity });
+        //console.log("Towers: " + towers.length);
+
+        //if room has towers, and no tower guard
         if (towers.length > 0 && towerGuards < 1) {
+            //create a towerguard
             var newName = 'Tower Guard ' + Game.time;
             console.log('Spawning new Guard: ' + newName);
             Game.spawns['HELL'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName,
                 { memory: { role: 'towerGuard' } });
-
+            //Fillers
         } else if (fillers.length < 0) {
 
             var newName = 'Filler' + Game.time;
             console.log('Spawning new Filler: ' + newName);
             Game.spawns['HELL'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, { memory: { role: 'filler' } });
-
+            //Harvesters
         } else if (harvesters.length < 4) {
 
             var newName = 'Harvester' + Game.time;
-            console.log('Spawning new harvester: ' + newName);
-            if (Hteam1.length < 1) {
+            console.log('Spawning new harvester: ' + Hteam1.length);
+
+            //create teams 0 and 1
+            if (Hteam1.length < 0) {
                 Game.spawns['HELL'].spawnCreep([WORK, WORK, WORK, CARRY, MOVE], newName, { memory: { role: 'harvester', team: 1 } });
             } else {
                 Game.spawns['HELL'].spawnCreep([WORK, WORK, CARRY, MOVE], newName, { memory: { role: 'harvester', team: 0 } });
             }
-
+            //Upgraders
         } else if (upgraders.length < 8) {
 
             var newName = "uppity" + Game.time;
             console.log("Spawning new uppity: " + newName);
             Game.spawns['HELL'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, { memory: { role: 'upgrader' } });
-
+            //Builders
         } else if (builders.length < 4) {
 
             var newName = "Bob" + Game.time;
             console.log("Spawning new bob the builder: " + newName);
-
+            //teams 1 and 2
             if (Bteam1.length > 1) {
                 Game.spawns['HELL'].spawnCreep([WORK, CARRY, WORK, MOVE], newName, { memory: { role: 'builder', building: false, team: 2 } });
                 //team: 2
@@ -136,19 +145,19 @@ module.exports.loop = function () {
                 Game.spawns['HELL'].spawnCreep([WORK, CARRY, CARRY, MOVE], newName, { memory: { role: 'builder', team: 1 } });
                 //team: 1
             }
-
-        } else if (repairers.length < 6) {
+            //Handys / Repairers
+        } else if (repairers.length < 3) {
 
             var newName = 'handy' + Game.time;
             console.log('Spawning new handy: ' + newName);
             Game.spawns['HELL'].spawnCreep([WORK, CARRY, CARRY, MOVE,], newName, { memory: { role: 'repairer' } });
-
+            //Settlers
         } else if (settlers.length < 0) {
 
             var newName = "columbus" + Game.time;
             console.log("spawning new settler: " + newName);
             Game.spawns['HELL'].spawnCreep([MOVE, MOVE, MOVE, CLAIM, CARRY, WORK], newName, { memory: { role: 'settler', room: myroom } });
-
+            //Importers
         } else if (importers.length < 0) {
 
             var newName = "import" + Game.time;
@@ -158,7 +167,7 @@ module.exports.loop = function () {
         }
     }
 
-    //display whena  new screep is spawning
+    //display when a new screep is spawning
     if (Game.spawns['HELL'].spawning) {
         var spawningCreep = Game.creeps[Game.spawns['HELL'].spawning.name];
         Game.spawns['HELL'].room.visual.text(
